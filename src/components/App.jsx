85d714f3c421f066581ef1d3131f6e9fec9d28d3
Pranav shelke <88943865+PranavShelke4@users.noreply.github.com>
2021-10-27 10:53:47 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,12 @@
 import React from "react";
 import Form from "./Form";
 
-var userIsRegistered = false;
-
-function renderConditionally() {
-  if (userIsRegistered === false) {
-    return <Form />;
-  } else {
-    return (
-      <>
-        <h1>Admin Login</h1>
-        {/* <img src="" /> */}
-        <form className="form">
-          <input type="text" placeholder="Username" />
-          <input type="password" placeholder="Password" />
-          <button type="submit">Login</button>
-        </form>
-      </>
-    );
-  }
-}
-
 function App() {
-  return <div className="container">{renderConditionally()}</div>;
+  return (
+    <div>
+      <Form />
+    </div>
+  );
 }
 
 export default App;
