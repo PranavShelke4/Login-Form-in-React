29d65a39cbe8fe5988b8336b1a2e273e7a6724bb
Pranav shelke <88943865+PranavShelke4@users.noreply.github.com>
2021-10-27 10:54:07 +0530

    Update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,13 +2,41 @@ import React from "react";
 
 function Form() {
   return (
-      <><h1>Admin Register</h1>
-      <form className="form">
-          <input type="text" placeholder="Username" />
-          <input type="password" placeholder="Password" />
-          <input type="password" placeholder="Confirm Password" />
-          <button type="submit">Register</button>
-      </form></>
+    <div className="white_box">
+      <div class="row" id="login_section">
+        <div className="icon">
+          <img
+            src="https://colorlib.com/etc/lf/Login_v1/images/img-01.png"
+            alt="IMG"
+          ></img>
+        </div>
+        <div className="login_form">
+          <h1>User Login</h1>
+
+          <input id="ip2" type="email" name="email" placeholder="Email" />
+          <br />
+          <input
+            id="ip2"
+            type="Password"
+            name="Password"
+            placeholder="Password"
+          />
+          <br />
+
+          <button type="submit">Login</button>
+
+          <p>
+            Forgot{" "}
+            <a class="txt2" href="#">
+              Username / Password?
+            </a>
+          </p>
+          <a class="txt2" href="#">
+            Create your Account
+          </a>
+        </div>
+      </div>
+    </div>
   );
 }
 
